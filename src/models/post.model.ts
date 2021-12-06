c2a800892404fd4536f4223361e483c91cacd01f
Clément HUGON <Clement.HUGON@diiage.org>
2021-12-06 10:17:30 +0100

    first commit

---
+++ src/models/post.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Post extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  slug: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  content: string;
+
+
+  constructor(data?: Partial<Post>) {
+    super(data);
+  }
+}
+
+export interface PostRelations {
+  // describe navigational properties here
+}
+
+export type PostWithRelations = Post & PostRelations;
